d26f381fa3c78cc5738c2ac1b9f16511d86f05de
Ahmed Mohamed <AhmedKaKashi74@gmail.com>
2019-11-19 15:35:40 +0200

    Initial Commit

---
+++ HolidayCalender/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HolidayCalender
+//
+//  Created by Ahmed on 11/19/19.
+//  Copyright © 2019 AhmedDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
